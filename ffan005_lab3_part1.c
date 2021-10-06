8e46883e55dd92e6e98df2d8b2eecc82667186d8
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-06 15:05:01 -0700

    Create ffan005_lab3_part1.c

---
+++ ffan005_lab3_part1.c
@@ -0,0 +1,17 @@
+echo ======================================================\n
+echo Running all tests..."\n\n
+
+# Example test:
+test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
+setPINA 0x00
+setPINB 0x00
+continue 2
+expectPORTC 0
+checkResult
+
+# Add tests below
+
+# Report on how many tests passed/tests ran
+set $passed=$tests-$failed
+eval "shell echo Passed %d/%d tests.\n",$passed,$tests
+echo ======================================================\n
