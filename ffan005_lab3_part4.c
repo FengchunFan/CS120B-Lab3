da535b16746597ed8966618beaa64b66a7b0e76b
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-06 18:33:03 -0700

    Create ffan005_lab3_part4.c

---
+++ ffan005_lab3_part4.c
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+DDRA = 0x00; PORTA = 0xFF;
+DDRC = 0x00; PORTC = 0x00;
+DDRB = 0x00; PORTB = 0x00;
+
+unsigned char tmpA = 0x00;
+unsigned char tmpB = 0x00;
+    /* Insert your solution below */
+    while (1) {
+    
+    }   
+    return 1;
+}
